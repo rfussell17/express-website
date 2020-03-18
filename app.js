d32312353e0f3c0e606c72a7be4cecb6fc4857de
Robin Fussell <rfussell17@gmail.com>
2020-03-18 12:21:48 -0700

    added project routes

--- app.js
+++ app.js
@@ -10,6 +10,41 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.render("home");
+});
+
+app.get("/projects", function(req, res){
+  res.render("home");
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+})
+
+app.get("/contact", function(req, res){
+  res.render("contact");
+})
+
+app.get("/projects/pomodoro", function(req, res){
+  res.render("pomodoro");
+})
+
+app.get("/projects/snake", function(req, res){
+  res.render("snake");
+})
+
+app.get("/projects/todo", function(req, res){
+  res.render("todo");
+})
+
+app.get("/projects/expense", function(req, res){
+  res.render("expense");
+})
+app.get("/projects/clock", function(req, res){
+  res.render("home");
+})
+
+app.get("/projects/chat", function(req, res){
+  res.render("home");
 })
 
 
